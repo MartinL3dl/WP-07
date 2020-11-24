56eb341eb9dc29860b73d8a318b7fb0e474ba80b
MartinL3dl <60381918+MartinL3dl@users.noreply.github.com>
2020-11-24 15:58:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,6 @@
   </head>
   <body>
  <?php
-$gamename[0] = "kokpiště";
-$gamename[1] = "konopiště";
-$gamename[2] = "koksoypisteD";
-$gamename[3] = "Friendzone 2 HD edition";
-$gamename[4] = "The Dankness";
-$gamename[5] = "The many of us whole II";
-
 $genre[0] = "FPS";
 $genre[1] = "RPG";
 $genre[2] = "Tahovka";
@@ -41,57 +34,48 @@ $timeplayed[2] = "víc jak 12 hodin";
 $timeplayed[3] = "víc jak tři dny (času dohromady)";
 $timeplayed[4] = "víc jak týden (času dohromady)";
 
-$gamer[0] = "XXX_pUsiDestroyer_XXX";
-$gamer[1] = "第五章 第四章 第九章 第六章 第二章 第十章";
-$gamer[2] = "Zyklo25nBen48";
-$gamer[3] = "Bezdomovec2000";
-$gamer[4] = "legendarySucc_er";
-$gamer[5] = "Gucciganger";
-
-/* to co se bude vypisovat, mohu changenout jak se zlíbí */
-
-$array[0]['Nick'] = $gamer[0];
-$array[0]['Game'] = $gamename[5];
+$array[0]['Nick'] = "Bezdomovce6969";
+$array[0]['Game'] = "power fantasy (home edition)";
 $array[0]['Genre'] = $genre[3];
 $array[0]['Subgenre'] = $subgenre[2];
 $array[0]['Platform'] = $platform[1];
 $array[0]['Skill'] = $skill[2];
 $array[0]['Timeplayed'] = $timeplayed[0];
 
-$array[1]['Nick'] = $gamer[3];
-$array[1]['Game'] = $gamename[3];
+$array[1]['Nick'] = "Smoki";
+$array[1]['Game'] = "REEEEEEEEEEEEEEEEEEEEEE";
 $array[1]['Genre'] = $genre[3];
 $array[1]['Subgenre'] = $subgenre[3];
 $array[1]['Platform'] = $platform[1];
 $array[1]['Skill'] = $skill[3];
 $array[1]['Timeplayed'] = $timeplayed[1];
 
-$array[2]['Nick'] = $gamer[4];
-$array[2]['Game'] = $gamename[4];
+$array[2]['Nick'] = "BellaDelphine";
+$array[2]['Game'] = "OnlyFans";
 $array[2]['Genre'] = $genre[0];
 $array[2]['Subgenre'] = $subgenre[4];
 $array[2]['Platform'] = $platform[4];
 $array[2]['Skill'] = $skill[4];
 $array[2]['Timeplayed'] = $timeplayed[4];
 
-$array[3]['Nick'] = $gamer[1];
-$array[3]['Game'] = $gamename[2];
+$array[3]['Nick'] = "NeilCuckmann";
+$array[3]['Game'] = "destroyer of dreams";
 $array[3]['Genre'] = $genre[3];
 $array[3]['Subgenre'] = $subgenre[4];
 $array[3]['Platform'] = $platform[2];
 $array[3]['Skill'] = $skill[2];
 $array[3]['Timeplayed'] = $timeplayed[1];
 
-$array[4]['Nick'] = $gamer[3];
-$array[4]['Game'] = $gamename[2];
+$array[4]['Nick'] = "INSERT_NAME";
+$array[4]['Game'] = "MR krabs overdoses on Ketamine and dies";
 $array[4]['Genre'] = $genre[3];
 $array[4]['Subgenre'] = $subgenre[4];
 $array[4]['Platform'] = $platform[0];
 $array[4]['Skill'] = $skill[1];
 $array[4]['Timeplayed'] = $timeplayed[4];
 
-$array[5]['Nick'] = $gamer[5];
-$array[5]['Game'] = $gamename[0];
+$array[5]['Nick'] = "SliceOf(in)bread";
+$array[5]['Game'] = "Team fortress 2";
 $array[5]['Genre'] = $genre[2];
 $array[5]['Subgenre'] = $subgenre[0];
 $array[5]['Platform'] = $platform[4];
